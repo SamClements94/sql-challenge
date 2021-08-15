5dff30eb760288735e8e56ea80bdad0f35d974c2
Sam Clements <82127462+SamClements94@users.noreply.github.com>
2021-08-15 16:20:50 -0700

    final

---
+++ Schema.sql
@@ -0,0 +1,43 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/jV4vEP
+
+CREATE TABLE "departments" (
+    "dept_no" VARCHAR(50)   NOT NULL,
+    "dept_name" VARCHAR(50)   NOT NULL,
+    CONSTRAINT "pk_departments" PRIMARY KEY (
+        "dept_no"
+     )
+);
+
+CREATE TABLE "employees" (
+    "emp_no" int   NOT NULL,
+    "emp_title_id" VARCHAR(50)   NOT NULL,
+    "birth_date" VARCHAR(50)   NOT NULL,
+    "first_name" VARCHAR(50)   NOT NULL,
+    "last_name" VARCHAR(50)   NOT NULL,
+    "sex" VARCHAR(50)   NOT NULL,
+    "hire_date" VARCHAR(50)   NOT NULL,
+    CONSTRAINT "pk_employees" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "dept_emp" (
+    "emp_no" INT   NOT NULL,
+    "dept_no" VARCHAR(50)   NOT NULL
+);
+
+CREATE TABLE "dept_manager" (
+    "dept_no" VARCHAR(50)   NOT NULL,
+    "emp_no" INT   NOT NULL
+);
+
+CREATE TABLE "salaries" (
+    "emp_no" INT   NOT NULL,
+    "salary" INT   NOT NULL
+);
+
+CREATE TABLE "titles" (
+    "title_id" VARCHAR(50)   NOT NULL,
+    "title" VARCHAR(50)   NOT NULL
+);
